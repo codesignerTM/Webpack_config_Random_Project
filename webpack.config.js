95e3c22d6f0c3b88ddb22e93226b2f83f6ba90f7
Tamás <codesigner.hu@gmail.com>
2020-02-11 22:03:27 +0100

    adding css-loader/style-loader/autoprefixer to handle css in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoPrefixer = require("autoprefixer");
 
 module.exports = {
   mode: "development",
@@ -15,6 +16,29 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         exlude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exlude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[name]_[local]_[hash:base64:5]"
+              }
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [autoPrefixer()]
+            }
+          }
+        ]
       }
     ]
   }
